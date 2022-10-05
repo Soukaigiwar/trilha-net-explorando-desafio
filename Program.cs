1327dbed85e1852077e7a0a1696787627c45d5bc
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2022-10-04 23:26:52 -0300

    projeto terminado

--- Program.cs
+++ Program.cs
@@ -8,15 +8,27 @@ List<Pessoa> hospedes = new List<Pessoa>();
 
 Pessoa p1 = new Pessoa(nome: "Hóspede 1");
 Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+Pessoa p3 = new Pessoa(nome: "Hóspede 3");
+Pessoa p4 = new Pessoa(nome: "Hóspede 4");
+Pessoa p5 = new Pessoa(nome: "Hóspede 5");
+Pessoa p6 = new Pessoa(nome: "Hóspede 6");
+Pessoa p7 = new Pessoa(nome: "Hóspede 7");
+Pessoa p8 = new Pessoa(nome: "Hóspede 8");
 
 hospedes.Add(p1);
 hospedes.Add(p2);
+hospedes.Add(p3);
+hospedes.Add(p4);
+hospedes.Add(p5);
+hospedes.Add(p6);
+hospedes.Add(p7);
+hospedes.Add(p8);
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suite = new Suite(tipoSuite: "Presidencial", capacidade: 6, valorDiaria: 880);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Reserva reserva = new Reserva(diasReservados: 11);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
